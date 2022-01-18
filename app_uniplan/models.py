99fb8425ec68dc853e7fea34bba1569489c16e97
Lewis Gentle <lewis.gentle@gmail.com>
2022-01-19 09:14:37 +1100

    Update models.py

--- app_uniplan/models.py
+++ app_uniplan/models.py
@@ -9,7 +9,8 @@ class Unit(models.Model):
 	the model for a unit
 	'''
 	name = models.CharField(max_length=200)
-	code = models.CharField(max_length=8, unique=True)
+	unit_code = models.CharField(max_length=8, unique=True, primary_key=True)
+	unitguideURL = models.URLField(blank=True)
 	description = models.TextField()
 	created_date = models.DateTimeField(default=timezone.now)
 	created_by = models.ForeignKey(User, on_delete=models.CASCADE)
