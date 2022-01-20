014f1857b551e724ad9517d1b651635ecc81aae1
Lewis Gentle <lewis.gentle@gmail.com>
2022-01-21 00:44:52 +1100

    Password Reset and Auth

--- uniplan/settings.py
+++ uniplan/settings.py
@@ -127,3 +127,5 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 LOGIN_REDIRECT_URL = 'index'
 LOGOUT_REDIRECT_URL = 'index'
+EMAIL_BACKEND = "django.core.mail.backends.filebased.EmailBackend"
+EMAIL_FILE_PATH = str(BASE_DIR.joinpath('sent_emails'))
