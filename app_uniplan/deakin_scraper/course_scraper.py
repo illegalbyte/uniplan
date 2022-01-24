849cf6f2ac1457ec5db3a84e222ed2509ba5c662
Lewis Gentle <lewis.gentle@gmail.com>
2022-01-25 10:31:24 +1100

    add majors/minors unit sequence scraper

--- app_uniplan/deakin_scraper/course_scraper.py
+++ app_uniplan/deakin_scraper/course_scraper.py
@@ -4,8 +4,15 @@ import os
 import pprint
 
 
-def deakin_handbook_scraper(url: str):
-
+def deakin_handbook_scraper(url: str) -> dict:
+	'''
+	Scrape the Deakin Handbook website for course information.
+	# Returns a dictionary of the following format:
+	# Example of a unit dictionary entry:
+	#  'STP050': {'unit_code': 'STP050',
+	#             'unit_name': 'Academic Integrity (0 credit points)',
+	#             'unitguideURL': 'http://www.deakin.edu.au/current-students-courses/unit.php?unit=STP050&year=2022&return_to=%2Fcurrent-students-courses%2Fcourse.php%3Fcourse%3DS326%26keywords%3Dbachelor%2Bof%2Binformation%2Btechnology%26version%3D2%26year%3D2022'}}
+	'''
 
 	# url = "https://www.deakin.edu.au/current-students-courses/course.php?course=S326&version=2&year=2022&keywords=bachelor+of+information+technology"
 
@@ -31,6 +38,7 @@ def deakin_handbook_scraper(url: str):
 
 	# use bs to find the "Core" h2 tag and then use find all tables after the H2 tag
 		#TODO: this solution may cause issues with other courses that have a different structure
+		#TODO: this does not actually filter by major, minor, etc, but includes all units mentioned on the course page
 	core_units_table = soup.find('h2', text='Core').find_all_next('table')
 
 	units_tables = {}
@@ -55,3 +63,29 @@ def deakin_handbook_scraper(url: str):
 	#  'STP050': {'unit_code': 'STP050',
 	#             'unit_name': 'Academic Integrity (0 credit points)',
 	#             'unitguideURL': 'http://www.deakin.edu.au/current-students-courses/unit.php?unit=STP050&year=2022&return_to=%2Fcurrent-students-courses%2Fcourse.php%3Fcourse%3DS326%26keywords%3Dbachelor%2Bof%2Binformation%2Btechnology%26version%3D2%26year%3D2022'}}
+
+
+def sequence_guide(url: str) -> list:
+	'''
+	Scrape a major/minor sequence page in the Deakin Handbook for units contained in that sequence.
+	Returns a list of touples the unit codes contained in the sequence.
+	'''
+	response = requests.get(url)
+	response.raise_for_status()
+	soup = bs.BeautifulSoup(response.text, 'html.parser')
+
+	all_units_tables = soup.find_all('table')
+
+	sequence_units = []
+
+	for table in all_units_tables: 
+		unit_code = table.find('td').text.strip()
+		unit_name = table.find('td').find_next('td').text
+		sequence_units.append(unit_code)
+
+	return sequence_units
+
+
+def test_sequence_guide():	
+	units = sequence_guide("https://www.deakin.edu.au/current-students-courses/detail.php?customer_cd=C&service_item=S326&version_number=2&element_cd=MAJORS-STRUCTURE&sub_item_number=14&year=2022&return_to=%2Fcurrent-students-courses%2Fcourse.php%3Fcourse%3DS326%26keywords%3Dbachelor%2Bof%2Binformation%2Btechnology%26version%3D2%26year%3D2022")
+	print(units)
