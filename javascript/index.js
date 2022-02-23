be92e661322efec234ef7cc607cc929d8732d04c
Lewis Gentle <lewis.gentle@gmail.com>
2022-02-24 01:59:55 +1100

    Tried React and nope...

---
+++ javascript/index.js
@@ -0,0 +1,4 @@
+// React gives me a migrain... maybe I'll return to it one day, for now I'm just going to use vanilla Js
+
+console.log("React actually isn't great.")
+
--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,19 @@
 const path = require('path');
 
 module.exports = {
-	entry: './javascript/assignments.js',  // path to our input file
+	entry: './javascript/index.js',  // path to our input file
 	output: {
 		filename: 'index-bundle.js',  // output bundle file name
 		path: path.resolve('./app_uniplan/static/js'),  // path to our Django static directory
 	},
+	module: {
+		rules: [
+			{
+				test: /\.(js|jsx)$/,
+				exclude: /node_modules/,
+				loader: "babel-loader",
+				options: { presets: ["@babel/preset-env", "@babel/preset-react"] }
+			},
+		]
+	}
 };
